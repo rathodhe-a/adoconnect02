ecb8729907fb1a4a041d8a2c844ae392b8df3dde
rathodhe-a <96012693+rathodhe-a@users.noreply.github.com>
2024-07-10 20:11:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
         resource_group_name = "Schnieder_hemant"  
         storage_account_name = "stogterraformstate01" 
         container_name = "terraformstatefile" 
-        key = "dev.terraform.tfstate"
-        key2 = "__tfstatefile__"
+        key = "__tfstatefile__"
       }
 }
